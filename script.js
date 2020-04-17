3da44ed50c46981e2c2c12ddfa470a726943c8c4
ChinaraGuliyeva <chinarai@yandex.ru>
2020-04-17 10:44:38 +0400

    gotr value

--- script.js
+++ script.js
@@ -5,21 +5,6 @@ let insideLoading = document.querySelector('.inside-loading');
 let loading = document.querySelector('.loading');
 let amountInput = document.querySelector('.amount');
 
-let currensyHandler = (e) =>{
-    e.target.style.backgroundColor = '#833AE0';
-    console.log(e.target.value);
-    getCurrency(e.target.value);
-    console.log(getCurrency(e.target.value));
-}
-
-async function getCurrency(currency) {
-    showLoading();
-    let result = await fetch(`https://api.ratesapi.io/api/latest?base=${currency}`);
-    let resultData = await result.json();
-    hideLoading();
-    return resultData;
-}
-
 const showLoading = () => {
     insideLoading.style.display='block';
     loading.style.display='block';
@@ -30,7 +15,38 @@ const hideLoading = () => {
     loading.style.display='none';
 }
 
-getCurrency('USD');
+async function getCurrency(currency) {
+    showLoading();
+    let result = await fetch(`https://api.ratesapi.io/api/latest?base=${currency}`);
+    let resultData = await result.json();
+    hideLoading();
+    return resultData;
+}
+
+const currensyHandler = (e) =>{
+    toggle(e.target);
+    console.log(e.target.value);
+    getCurrency(e.target.value);
+    console.log(getCurrency(e.target.value));
+   //console.log(updateValue(e));
+
+}
+
+//asynch?
+const currencyCalculator = (e) => {
+    toggle(e.target);
+    console.log(e.target.value);
+}
+
+const toggle = (el) => {
+    el.style.backgroundColor = (el.style.backgroundColor == 'white') ? '#833AE0' : 'white'
+  }
+
+let updateValue =(e)=>{
+    return e.target.value;
+}
 
 buttons.forEach(element => element.addEventListener('click', currensyHandler));
+buttons2.forEach(element => element.addEventListener('click', currencyCalculator));
+amountInput.addEventListener('change', updateValue);
 
